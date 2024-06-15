9ba0b92a20e5c9f211051d8a52f182bd843b39a8
Edwin <me@edwin.one>
2024-06-15 10:55:43 +0330

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
-# to be used with client-connect option of openvpn
-#Debug mode
 
-#exec 2> script_debug
-#set -x
-
-# Simple dad by checking all files in pool dir
+# Function to check for duplicate addresses
 dad_check () {
-        local test_addr=$1
-        for i in ls $_pool_ipv6_dir; do
-                [ $i = $common_name ] && continue
-                [ $(< $_pool_ipv6_dir/$i) = $test_addr ] && return 1
-        done
-        return 0
+    local test_addr=$1
+    for i in $(ls $_pool_ipv6_dir); do
+        [ "$i" = "$common_name" ] && continue
+        [ "$(cat $_pool_ipv6_dir/$i)" = "$test_addr" ] && return 1
+    done
+    return 0
 }
 
+# Function to generate a new IPv6 address
 gen_ipv6_addr () {
-        local prefix=$1
-        local addr=$prefix$(dd if=/dev/random bs=1 count=8 2> /dev/null | xxd -p|sed -re 's/(.{4})/:\1/g')
-        dad_check $addr || addr=$(gen_ipv6_addr $prefix)
-        echo $addr
+    local prefix=$1
+    local addr
+    while true; do
+        addr=$prefix$(dd if=/dev/random bs=1 count=8 2> /dev/null | xxd -p | sed -re 's/(.{4})/:\1/g')
+        dad_check $addr && break
+    done
+    echo $addr
 }
 
+# Main script execution
 _config_file="$1"
-
 _timeout=3600
 
-#_prefix_ipv6="2001:bc8:2869:161"
+# Extract prefix from ifconfig_ipv6_local
 _prefix_ipv6=${ifconfig_ipv6_local%%::*}
 _pool_ipv6_dir=/var/lib/openvpn/pool_$_prefix_ipv6
-
 _pool_ipv6=$_pool_ipv6_dir/$common_name
 
-[ -d $_pool_ipv6_dir ] || mkdir $_pool_ipv6_dir
+# Ensure pool directory exists
+[ -d $_pool_ipv6_dir ] || mkdir -p $_pool_ipv6_dir
 
-if [ -f $_pool_ipv6 ] && [ $(stat --printf %Y $_pool_ipv6) -ge $(date -d-${_timeout}sec +%s) ]; then
-        _addr_ipv6=$(< $_pool_ipv6)
-        dad_check $_addr_ipv6 || { _addr_ipv6=$(gen_ipv6_addr $_prefix_ipv6) && echo -n $_addr_ipv6 > $_pool_ipv6; }
+# Check if there's an existing valid address
+if [ -f $_pool_ipv6 ] && [ "$(stat --printf %Y $_pool_ipv6)" -ge "$(date -d-${_timeout}sec +%s)" ]; then
+    _addr_ipv6=$(< $_pool_ipv6)
+    dad_check $_addr_ipv6 || _addr_ipv6=$(gen_ipv6_addr $_prefix_ipv6)
 else
-        _addr_ipv6=$(gen_ipv6_addr $_prefix_ipv6)
-        echo -n $_addr_ipv6 > $_pool_ipv6
+    _addr_ipv6=$(gen_ipv6_addr $_prefix_ipv6)
 fi
 
+# Save the address and update the config file
+echo -n $_addr_ipv6 > $_pool_ipv6
 echo "ifconfig-ipv6-push $_addr_ipv6" > $_config_file
 
-#exit 0
+# Exit successfully
+exit 0
